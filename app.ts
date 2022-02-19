704c716816936a5268ae69c0f58d9287ef1fb0aa
ANTHONY SCARDAPANE <an.scardapane@gmail.com>
2022-02-19 14:27:23 -0500

    initial api

---
+++ app.ts
@@ -0,0 +1,67 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import "https://deno.land/x/dotenv/load.ts";
+
+interface PageViewCount {
+    count: number,
+    resource: string
+}
+
+interface NetlifyResponse {
+    data: Map<number, PageViewCount>
+}
+
+export const NETLIFY_TOKEN = Deno.env.get("NETLIFY_TOKEN")
+export const NETLIFY_SITE_ID = Deno.env.get("NETLIFY_SITE_ID")
+
+const router = new Router();
+const netlifyEndPoint = `https://analytics.services.netlify.com/v2/${NETLIFY_SITE_ID}/ranking/pages?from=1642654800000&to=1645294136742&timezone=-0500&limit=15`
+
+//GET BLOG VIEW COUNT
+router.get('/blog/:title', async (context) => {
+
+    //GET NETLIFY ANALYTICS
+    const response = await fetch(netlifyEndPoint, {
+        headers: {
+            Authorization: `Bearer ${NETLIFY_TOKEN}`,
+        },
+    });
+
+    const dataList: NetlifyResponse  = await response.json();
+    const blogPostMap = new Map();
+
+    for (const [, postDetail] of dataList.data.entries()) {
+        const count = postDetail.count;
+        const path = postDetail.resource;
+
+        if(path.includes('/posts/')) {
+            const blogPost = path.slice(7, path.length - 1) //yea whatever
+            blogPostMap.set(blogPost,count)
+        }
+    }
+    const blogTitle = context.params.title
+    context.response.body = {
+        viewCount: blogPostMap.get(blogTitle)
+    }
+})
+
+const app = new Application();
+
+// Logger
+app.use(async (ctx, next) => {
+    await next();
+    const rt = ctx.response.headers.get("X-Response-Time");
+    console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+// Timing
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
